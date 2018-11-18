d3d41b0b9ffc284132517064020f36daee7fbe8a
Rebekah Lane <gadgetgirlmommy@gmail.com>
2018-11-18 15:51:49 -0600

    docs update

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react'
-
-class Listing extends Component {
-  render() {
-    //if no available image, display filler
-    let displayedImage = (this.props.listing.image_url !== "") ?
-      this.props.listing.image_url : 'http://via.placeholder.com/250x250.jpg/FFFFFF/000000/?text=No+Image+Available';
-
-    return (
-      <section
-      // sets the corresponding marker animation
-        onClick={() => this.props.toggleListingMarker(this.props.index)} 
-        className='listing' >
-        <div tabIndex='0' className="listingName">{this.props.listing.name}</div>
-        <div className="listingPhoto">
-          <img alt={this.props.listing.name + " photo"} src={displayedImage} />
-        </div>
-        <div>{this.props.listing.address[0]}</div>
-        <div>{this.props.listing.address[1]}</div>
-        <div><a href={this.props.listing.url}>See Yelp For More Information</a></div>
-      </section>
-    )
-  }
-}
-
-export default Listing
-||||||| merged common ancestors
-import React, { Component } from 'react'
-
-class Listing extends Component {
-  render() {
-    //if no available image, display filler
-    let displayedImage = (this.props.listing.image_url !== "") ?
-      this.props.listing.image_url : 'http://via.placeholder.com/250x250.jpg/FFFFFF/000000/?text=No+Image+Available';
-
-    return (
-      <section
-      // sets the corresponding marker animation
-        onClick={() => this.props.toggleListingMarker(this.props.index)} 
-        className='listing' >
-        <div tabIndex='0' className="listingName">{this.props.listing.name}</div>
-        <div className="listingPhoto">
-          <img alt={this.props.listing.name + " photo"} src={displayedImage} />
-        </div>
-        <div>{this.props.listing.address[0]}</div>
-        <div>{this.props.listing.address[1]}</div>
-        <div><a href={this.props.listing.url}>See Yelp For More Information</a></div>
-      </section>
-    )
-  }
-}
-
-export default Listing
-=======
 import React, { Component } from 'react'
 
 class Listing extends Component {
@@ -84,4 +29,3 @@ class Listing extends Component {
 }
 
 export default Listing
->>>>>>> listing-changes
