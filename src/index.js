e49ff7763883c8f9d93d5b42bfdfda061e26cd9b
Rebekah Lane <gadgetgirlmommy@gmail.com>
2018-11-19 09:53:06 -0600

    fix: Added code to remove material-ui error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+// The code below was taken from the material-ui page. Because I am not using a theme of any sort, 
+// I put this code in place to remove the error being logged to the console about depriciation. 
+// Reference can be found here:
+// https://material-ui.com/style/typography/#migration-to-typography-v2 and here:
+// https://github.com/mui-org/material-ui/issues/13366
+window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true; 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
